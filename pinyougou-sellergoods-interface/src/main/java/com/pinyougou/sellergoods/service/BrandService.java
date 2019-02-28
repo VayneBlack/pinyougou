2ad10dc3fc9dd508ea6014bf193f25526e613cf6
tom <tom@qq.com>
2019-02-28 14:40:43 +0800

    做到添加

---
+++ pinyougou-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/service/BrandService.java
@@ -0,0 +1,28 @@
+package com.pinyougou.sellergoods.service;
+
+import java.util.List;
+
+import com.pinyougou.pojo.TbBrand;
+
+import entity.PageResult;
+
+/**
+ * 品牌接口
+ * @author admin
+ *
+ */
+public interface BrandService {
+	
+	public List<TbBrand> findAll();
+	
+	/**
+	 * 品牌分页
+	 * @param pageNum 当前页面
+	 * @param pageSize 每页记录数
+	 * @return
+	 */
+	public PageResult findPage(int pageNum,int pageSize);
+	
+	public void add(TbBrand brand);
+
+}
